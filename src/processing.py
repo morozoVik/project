ea0ab70d7b2b7850a11584d93fde4cc93df59582
Илья Морозов <ulb9@mail.ru>
2025-06-03 00:20:28 +0300

    испльзовал black

--- src/processing.py
+++ src/processing.py
@@ -6,15 +6,15 @@
 from typing import List, Dict
 
 
-def filter_by_state(transactions: List[Dict], state: str = 'EXECUTED') -> List[Dict]:
+def filter_by_state(transactions: List[Dict], state: str = "EXECUTED") -> List[Dict]:
     """Фильтрует список операций по указанному статусу."""
-    
+
     filtered_operations = []
-    
+
     for operation in transactions:
-        if operation.get('state') == state:
+        if operation.get("state") == state:
             filtered_operations.append(operation)
-            
+
     return filtered_operations
 
 
@@ -34,4 +34,4 @@ def sort_by_date(transactions: List[Dict], reverse: bool = True) -> List[Dict]:
         >>> sort_by_date(ops)
         [{'date': '2023-01-02'}, {'date': '2023-01-01'}]
     """
-    return sorted(transactions, key=lambda x: x['date'], reverse=reverse)
+    return sorted(transactions, key=lambda x: x["date"], reverse=reverse)
