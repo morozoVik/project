97e2af1acea605f60466de0607fb7f0cdb27e133
Admin <ulb9@mail.ru>
2025-06-26 23:04:02 +0300

    исправил ошибки, удалил гитигнор лишний

--- tests/tests/test_processing.py
+++ tests/tests/test_processing.py
@@ -1,4 +1,5 @@
 import pytest
+
 from src.processing import filter_by_state, sort_by_date
 
 
@@ -21,10 +22,10 @@ def test_filter_by_state(sample_transactions):
     result = filter_by_state(sample_transactions, "EXECUTED")
     assert len(result) == 2
     assert all(item["state"] == "EXECUTED" for item in result)
-    
+
     # Проверка, что исходный список не изменился
     assert len(sample_transactions) == 4
-    
+
     # Тестирование фильтрации по несуществующему статусу
     result = filter_by_state(sample_transactions, "UNKNOWN")
     assert len(result) == 0
@@ -34,7 +35,7 @@ def test_filter_by_state(sample_transactions):
     "reverse, expected_order",
     [
         (False, [1, 3, 2, 4]),  # Сортировка по возрастанию даты
-        (True, [4, 2, 3, 1]),   # Сортировка по убыванию даты
+        (True, [4, 2, 3, 1]),  # Сортировка по убыванию даты
     ],
 )
 def test_sort_by_date(sample_transactions, reverse, expected_order):
