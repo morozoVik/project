ff2dcda2b1a1823fde9b734dd60a03183e643e6d
Admin <ulb9@mail.ru>
2025-06-27 12:47:20 +0300

    исправил и добавил описание тестов реадми файл

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,6 +1,8 @@
-import pytest
 from datetime import datetime
-from src.widget import mask_account_card, get_date
+
+import pytest
+
+from src.widget import get_date, mask_account_card
 
 
 # Фикстуры для тестов mask_account_card
